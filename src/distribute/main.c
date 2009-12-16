6c3dac05ef5f9df0ce866d64699e200e2ec9ef93
Sander van der Burg <svanderburg@gmail.com>
2009-12-16 14:16:33 +0000

    Refactored all distribution export in a seperate module, since they are shared across multiple tools

--- src/distribute/main.c
+++ src/distribute/main.c
@@ -2,107 +2,10 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define _GNU_SOURCE
 #include <getopt.h>
 #include <glib.h>
-#include <libxml/parser.h>
-#include <libxml/xpath.h>
-
-typedef struct
-{
-    gchar *profile;
-    gchar *target;
-}
-DistributionItem;
-
-static xmlXPathObjectPtr executeXPathQuery(xmlDocPtr doc, char *xpath)
-{
-    xmlXPathContextPtr context;
-    xmlXPathObjectPtr result;
-    
-    context = xmlXPathNewContext(doc);
-    result = xmlXPathEvalExpression(xpath, context);
-    xmlXPathFreeContext(context);
-    
-    if(xmlXPathNodeSetIsEmpty(result->nodesetval))
-    {
-        xmlXPathFreeObject(result);
-        return NULL;
-    }
-    else
-	return result;
-}
-
-static GArray *generate_distribution_array(char *distribution_export_file)
-{
-    xmlDocPtr doc;
-    xmlNodePtr node_root;
-    xmlXPathObjectPtr result;
-    GArray *distribution_array = NULL;
-    unsigned int i;
-    
-    /* Parse the XML document */
-    
-    if((doc = xmlParseFile(distribution_export_file)) == NULL)
-    {
-	fprintf(stderr, "Error with parsing the distribution export XML file!\n");
-	return NULL;
-    }
-
-    /* Retrieve root element */
-    node_root = xmlDocGetRootElement(doc);
-    
-    if(node_root == NULL)
-    {
-        fprintf(stderr, "The distribution export XML file is empty!\n");
-	xmlFreeDoc(doc);
-	return NULL;
-    }
-    
-    /* Query the distribution elements */
-    result = executeXPathQuery(doc, "/distributionexport/distribution/mapping");
-    
-    /* Iterate over all the distribution elements and add them to the array */
-    
-    if(result)
-    {
-	xmlNodeSetPtr nodeset = result->nodesetval;
-	
-	/* Create a distribution array */
-        distribution_array = g_array_new(FALSE, FALSE, sizeof(DistributionItem*));
-    
-	/* Iterate over all the mapping elements */
-	for(i = 0; i < nodeset->nodeNr; i++)
-        {
-	    xmlNodePtr mapping_children = nodeset->nodeTab[i]->children;
-	    DistributionItem *item = (DistributionItem*)g_malloc(sizeof(DistributionItem));
-	    gchar *profile, *target;
-	    
-	    /* Iterate over all the mapping item children (profile and target elements) */
-	    
-	    while(mapping_children != NULL)
-	    {
-		if(xmlStrcmp(mapping_children->name, (xmlChar*) "profile") == 0)
-		    profile = g_strdup(mapping_children->children->content);
-		if(xmlStrcmp(mapping_children->name, (xmlChar*) "target") == 0)
-		    target = g_strdup(mapping_children->children->content);
-		
-		mapping_children = mapping_children->next;
-	    }
-	    
-	    /* Added the mapping to the array */
-	    item->profile = profile;
-	    item->target = target;
-	    g_array_append_val(distribution_array, item);
-        }
-    }
-    
-    /* Cleanup */    
-    xmlXPathFreeObject(result);
-    xmlFreeDoc(doc);
-
-    /* Return the distribution array */
-    return distribution_array;
-}
+#include "distributionmapping.h"
 
 static void print_usage()
 {
@@ -177,7 +80,6 @@ int main(int argc, char *argv[])
 	    
 	    /* Cleanups */
 	    g_free(command);
-	    g_free(item); /* Free the distribution item, since we don't need it anymore */
 	    
 	    /* On error stop the distribute process */
 	    if(status == -1)
