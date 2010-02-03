53d3b7f5c9d42d358787bbf1c8397bf2cab5b0a9
Sander van der Burg <svanderburg@gmail.com>
2010-02-03 15:47:37 +0000

    - Added stuff to the automake files so that everything will be included in a distribution - Added Nix expression for buildfarm

---
+++ release.nix
@@ -0,0 +1,39 @@
+{ nixpkgs ? /etc/nixos/nixpkgs }:
+
+let
+
+  jobs = rec {
+
+
+    tarball =
+      { disnix ? {outPath = ./.; rev = 1234;}
+      , officialRelease ? false
+      }:
+
+      with import nixpkgs {};
+
+      releaseTools.sourceTarball {
+        name = "disnix-tarball";
+        version = builtins.readFile ./version;
+        src = disnix;
+        inherit officialRelease;
+
+        buildInputs = [ pkgconfig dbus_glib libxml2 libxslt openssl ];
+      };
+
+
+    build =
+      { tarball ? jobs.tarball {}
+      , system ? "x86_64-linux"
+      }:
+
+      with import nixpkgs {inherit system;};
+
+      releaseTools.nixBuild {
+        name = "disnix";
+        src = tarball;
+
+        buildInputs = [ pkgconfig dbus_glib libxml2 libxslt openssl ];
+      };
+  };
+in jobs
