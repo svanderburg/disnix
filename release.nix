060d3cbc3a25fd796ed57c2f4929c62bf002e788
Sander van der Burg <svanderburg@gmail.com>
2010-10-26 20:27:38 +0000

    Implemented test suite for distributed building

--- release.nix
+++ release.nix
@@ -542,6 +542,50 @@ let
 	      }	      
 	    '';
         };
+	
+	distbuild = simpleTest {
+	  nodes = {
+	    coordinator = machine;
+	    testtarget1 = machine;
+	    testtarget2 = machine;
+	  };
+	  testScript =
+	    ''
+	      startAll;
+	      
+	      # Initialise ssh stuff by creating a key pair for communication
+	      my $key=`${pkgs.openssh}/bin/ssh-keygen -t dsa -f key -N ""`;
+    
+              $testtarget1->mustSucceed("mkdir -m 700 /root/.ssh");
+              $testtarget1->copyFileFromHost("key.pub", "/root/.ssh/authorized_keys");
+
+              $testtarget2->mustSucceed("mkdir -m 700 /root/.ssh");
+              $testtarget2->copyFileFromHost("key.pub", "/root/.ssh/authorized_keys");
+	      
+	      $coordinator->mustSucceed("mkdir -m 700 /root/.ssh");
+              $coordinator->copyFileFromHost("key", "/root/.ssh/id_dsa");
+              $coordinator->mustSucceed("chmod 600 /root/.ssh/id_dsa");
+	      
+	      # Deploy the complete environment and build all the services on
+	      # the target machines. This test should succeed.
+	      my $result = $coordinator->mustSucceed("NIXPKGS_ALL=${nixpkgs}/pkgs/top-level/all-packages.nix SSH_OPTS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' disnix-env --build-on-targets -s ${manifestTests}/services-complete.nix -i ${manifestTests}/infrastructure.nix -d ${manifestTests}/distribution-simple.nix");
+	      
+	      # Checks whether the testService1 has been actually built on the
+	      # targets by checking the logfiles. This test should succeed.
+	      
+	      $testtarget1->mustSucceed("[ \"\$(grep \"\\-testService1.drv\" /var/log/upstart/disnix)\" != \"\" ]");
+	      
+	      # Checks whether the build log of testService1 is available on the
+	      # coordinator machine. This test should fail, since the build
+	      # is performed on a target machine.
+	      
+	      my @closure = split('\n', $coordinator->mustSucceed("nix-store -qR result"));
+	      my @testService1 = grep(/\-testService1/, @closure);
+	      	      
+	      $coordinator->mustFail("nix-store -l @testService1");
+	    '';
+	};
+
       };
   };
 in jobs
