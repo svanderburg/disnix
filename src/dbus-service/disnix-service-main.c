65e6e0c01d59cd2fc3848eb72d91a489f0d8a125
Sander van der Burg <svanderburg@gmail.com>
2015-05-29 13:39:08 +0000

    Create proper help pages for disnix-service and disnix-client

--- src/dbus-service/disnix-service-main.c
+++ src/dbus-service/disnix-service-main.c
@@ -26,11 +26,33 @@
 
 static void print_usage(const char *command)
 {
-    fprintf(stderr, "Usage:\n");
-    fprintf(stderr, "%s [options]\n\n", command);
-    fprintf(stderr, "Options:\n");
-    fprintf(stderr, "--session-bus             Register the Disnix service on the session bus instead of the system bus (useful for testing)\n");
-    fprintf(stderr, "--help                    Shows the usage of this command to the user\n");
+    printf("Usage: %s [OPTION]\n\n", command);
+    
+    printf("The `disnix-service' tool is a daemon running on either the D-Bus system or\n");
+    printf("session bus, which provides remote access to various deployment operations,\n");
+    printf("such as importing, exporting, activating and deactivating services.\n\n");
+
+    printf("The daemon is not very useful on its own, since it requires a wrapper that\n");
+    printf("exposes the methods to remote users. The simplest wrapper that can be used is\n");
+    printf("just running a SSH server and by using the `disnix-ssh-client' from the client\n");
+    printf("machines.\n\n");
+    
+    printf("Other wrappers can also be used, which are basically thin layers that map the\n");
+    printf("RPC protocol calls to D-Bus calls. A web service layer and client, for instance,\n");
+    printf("is also available from the Disnix webpage which allows a user to use SOAP for\n");
+    printf("executing deployment operations instead of SSH.\n\n");
+    
+    printf("Options:\n");
+    printf("      --session-bus  Register the Disnix service on the session bus instead of\n");
+    printf("                     the system bus (useful for testing)\n");
+    printf("  -h, --help         Shows the usage of this command to the user\n");
+    printf("  -v, --version      Shows the version of this command to the user\n");
+}
+
+static void print_version(const char *command)
+{
+    printf("%s (" PACKAGE_NAME ") " PACKAGE_VERSION "\n\n", command);
+    printf("Copyright (C) 2008-2015 Sander van der Burg\n");
 }
 
 int main(int argc, char *argv[])
@@ -39,26 +61,30 @@ int main(int argc, char *argv[])
     int c, option_index = 0;
     struct option long_options[] =
     {
-	{"session-bus", no_argument, 0, 's'},
-	{"help", no_argument, 0, 'h'},
-	{0, 0, 0, 0}
+        {"session-bus", no_argument, 0, 's'},
+        {"help", no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'v'},
+        {0, 0, 0, 0}
     };
     
     int session_bus = FALSE;
     
     /* Parse command-line options */
-    while((c = getopt_long(argc, argv, "h", long_options, &option_index)) != -1)
+    while((c = getopt_long(argc, argv, "hv", long_options, &option_index)) != -1)
     {
-	switch(c)
-	{
-	    case 's':
-		session_bus = TRUE;
-		break;
-	    case 'h':
-	    case '?':
-		print_usage(argv[0]);
-		return 0;
-	}
+        switch(c)
+        {
+            case 's':
+                session_bus = TRUE;
+                break;
+            case 'h':
+            case '?':
+                print_usage(argv[0]);
+                return 0;
+            case 'v':
+                print_version(argv[0]);
+                return 0;
+        }
     }
 
     /* Start the program with the given options */
