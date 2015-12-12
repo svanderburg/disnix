80c75a7c27640c17a8e7854d248fa8c52abfc242
Sander van der Burg <svanderburg@gmail.com>
2015-12-12 13:47:34 +0000

    Use default file permissions for the logfiles

--- src/dbus-service/methods.c
+++ src/dbus-service/methods.c
@@ -132,7 +132,7 @@ static int open_log_file(const gint pid)
     
     mkdir(logdir, 0755);
     log_path = g_strconcat(logdir, "/", pidStr, NULL);
-    log_fd = open(log_path, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    log_fd = open(log_path, O_CREAT | O_EXCL | O_RDWR);
     
     g_free(log_path);
     return log_fd;
